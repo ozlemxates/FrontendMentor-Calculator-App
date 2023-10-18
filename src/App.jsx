3a5a87ff72663b40a2fa0d92e839998d19fdbb78
Özlem Ateş <ozlemxatess@outlook.com>
2023-10-18 15:35:32 +0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header';
+import Result from './components/Result';
+import Calc from './components/Calc';
+
+function App() {
+
+ 
+}
+
+export default App;
