835339c8970c0ec2bffe46e41162fa1bdd64376a
Özlem Ateş <ozlemxatess@outlook.com>
2024-04-22 14:41:58 +0300

    Add box Shadow

--- src/components/Calc.jsx
+++ src/components/Calc.jsx
@@ -9,22 +9,22 @@ const Calc = ({ onKeyPress }) => {
     return (
         <main className="Calc">
             <div className="grid-container">
-                <div className="grid-item" onClick={() => handleOperation("7")}>7</div>
-                <div className="grid-item" onClick={() => handleOperation("8")}>8</div>
-                <div className="grid-item" onClick={() => handleOperation("9")}>9</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("7")}>7</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("8")}>8</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("9")}>9</div>
                 <div className="grid-item cyan-bg" onClick={() => handleOperation("DEL")}>DEL</div>
-                <div className="grid-item" onClick={() => handleOperation("4")}>4</div>
-                <div className="grid-item" onClick={() => handleOperation("5")}>5</div>
-                <div className="grid-item" onClick={() => handleOperation("6")}>6</div>
-                <div className="grid-item" onClick={() => handleOperation("+")}>+</div>
-                <div className="grid-item" onClick={() => handleOperation("1")}>1</div>
-                <div className="grid-item" onClick={() => handleOperation("2")}>2</div>
-                <div className="grid-item" onClick={() => handleOperation("3")}>3</div>
-                <div className="grid-item" onClick={() => handleOperation("-")}>-</div>
-                <div className="grid-item" onClick={() => handleOperation(".")}>.</div>
-                <div className="grid-item" onClick={() => handleOperation("0")}>0</div>
-                <div className="grid-item" onClick={() => handleOperation("/")}>/</div>
-                <div className="grid-item" onClick={() => handleOperation("*")}>x</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("4")}>4</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("5")}>5</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("6")}>6</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("+")}>+</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("1")}>1</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("2")}>2</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("3")}>3</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("-")}>-</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation(".")}>.</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("0")}>0</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("/")}>/</div>
+                <div className="grid-item wht-hover" onClick={() => handleOperation("*")}>x</div>
                 <div className="grid-item span-2 cyan-bg" onClick={() => handleOperation("RESET")}>RESET</div>
                 <div className="grid-item span-2 red-bg" onClick={() => handleOperation("=")}>=</div>
             </div>
