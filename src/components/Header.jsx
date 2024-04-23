872b9aba1b1cb3c741b9b76e6505b62870626e3d
Özlem Ateş <ozlemxatess@outlook.com>
2024-04-23 16:04:32 +0300

    Improved theme selection button apperance

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,25 +4,18 @@ import './Header.css';
 const Header = () => {
     return (
         <main className="Header">
-            <div className="text">
+            <div className="logo">
                 <h1>calc</h1>
-                <h4>THEME</h4>
             </div>
             <div className="select-theme">
-                <div className="label">
-                    <label for="theme1">1</label>
-                    <label for="theme2">2</label>
-                    <label for="theme3">3</label>    
-                </div>
-                <div className="input">
+                <h4>THEME</h4>
+                <div className="radio-buttons">
                     <input id="theme1" type="radio" />
                     <input id="theme2" type="radio" />
-                    <input id="theme3" type="radio" />
+                    <input id="theme3" type="radio" /> 
                 </div>
             </div>
-          
         </main>
-      
     )
 }
 
